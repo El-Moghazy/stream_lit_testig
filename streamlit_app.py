d76abbe40935a392bae6104c58127ef165207ff8
el-moghazy <abdelrhman.elmoghazy@gmail.com>
2022-12-19 16:21:42 +0200

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,5 +18,7 @@ sheet_url = st.secrets["public_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 # Print results.
+st.text_input(label, value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, *, placeholder=None, disabled=False, label_visibility="visible")
+st.write("STOP WRITING B2A")
 for row in rows:
     st.write(f"{row.wholesaler_name} has a :{row.wholesaler_id_number}:")
